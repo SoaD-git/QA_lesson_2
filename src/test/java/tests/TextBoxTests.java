308d78456f69289ace44d7292fa11fa7d4d226ec
Dmitriy Lupachev <soadscod@gmail.com>
2020-11-14 21:29:26 +0300

    v1.0

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,72 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+
+        $("#userName").val("Alex");
+        $("#userEmail").val("Egorov@alex.com");
+        $("#currentAddress").val("Montenegro");
+        $("#permanentAddress").val("Street 1");
+        $("#submit").scrollTo().click();
+
+        $("#output").shouldHave(text("Name:Alex\n" +
+                "Email:Egorov@alex.com\n" +
+                "Current Address :Montenegro\n" +
+                "Permananet Address :Street 1"));
+    }
+
+    @Test
+    void fillFormWithVariablesTest() {
+        String userName = "Alex",
+                email = "Egorov@alex.com",
+                currentAddress = "Montenegro",
+                permanentAddress = "Street 1";
+
+
+        open("https://demoqa.com/text-box");
+
+        $("#userName").val(userName);
+        $("#userEmail").val(email);
+        $("#currentAddress").val(currentAddress);
+        $("#permanentAddress").val(permanentAddress);
+        $("#submit").scrollTo().click();
+
+        $("#output").shouldHave(text(
+                "Name:" + userName + "\n" +
+                "Email:Egorov@alex.com\n" +
+                "Current Address :Montenegro\n" +
+                "Permananet Address :Street 1"));
+
+        $("#output").shouldHave(text(String.format(
+                "Name:%s\n" +
+                "Email:%s\n" +
+                "Current Address :Montenegro\n" +
+                "Permananet Address :Street 1", userName, email)));
+
+        $("#name").shouldHave(text(userName));
+    }
+
+
+    @Test
+    void wrongEmailTest() {
+        open("https://demoqa.com/text-box");
+
+        $("#userName").val("Alex");
+        $("#userEmail").val("Egorov");
+        $("#currentAddress").val("Montenegro");
+        $("#permanentAddress").val("Street 1");
+        $("#submit").scrollTo().click();
+
+        $("#userEmail").shouldHave(cssClass("field-error"));
+    }
+}
